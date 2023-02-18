197c2e40ccc3c53af8d4fa016ea9e3e094361124
Spencer Comfort <109806759+GiddyGoatGaming@users.noreply.github.com>
2023-02-18 14:02:34 -0500

    Update envutil.js

--- src/commons/envutil.js
+++ src/commons/envutil.js
@@ -124,7 +124,7 @@ export function secondaryDohResolver() {
   return envManager.get("CF_DNS_RESOLVER_URL_2");
 }
 
-export function secondaryDohResolver() {
+export function tertiaryDohResolver() {
   if (!envManager) return null;
 
   return envManager.get("CF_DNS_RESOLVER_URL_3");
@@ -153,7 +153,7 @@ export function dohResolvers() {
   if (isWorkers()) {
     // upstream to two resolvers on workers; since egress is free,
     // faster among the 2 should help lower tail latencies at zero-cost
-    return [primaryDohResolver(), secondaryDohResolver()];
+    return [primaryDohResolver(), secondaryDohResolver(), tertiaryDohResolver()];
   }
 
   return [primaryDohResolver()];

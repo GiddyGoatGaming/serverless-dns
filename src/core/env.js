08562186a2c0e47cc9548e2806aaf8f475f68141
Spencer Comfort <109806759+GiddyGoatGaming@users.noreply.github.com>
2023-02-19 18:09:31 -0500

    Update env.js

--- src/core/env.js
+++ src/core/env.js
@@ -121,12 +121,12 @@ const defaults = new Map(
     // secondary doh upstream
     CF_DNS_RESOLVER_URL_2: {
       type: "string",
-      default: "h3://dns.nextdns.io/",
+      default: "h3://dns.nextdns.io/c556eb",
     },
     // tertiary doh upstream
     CF_DNS_RESOLVER_URL_3: {
       type: "string",
-      default: "h3://cloudflare-dns.com/dns-query",
+      default: "h3://c556eb.dns.nextdns.io",
     },
     // upstream recursive rethinkdns resolver running on Fly.io
     MAX_DNS_RESOLVER_URL: {

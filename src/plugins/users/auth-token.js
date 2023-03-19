a6fa097071c789aa619662391f893ceb3450b434
Spencer C <109806759+SpencerIsGiddy@users.noreply.github.com>
2023-03-19 09:05:21 -0400

    Update auth-token.js

--- src/plugins/users/auth-token.js
+++ src/plugins/users/auth-token.js
@@ -135,15 +135,15 @@ export async function gen(msg, domain) {
 // stackoverflow.com/a/47332317
 async function proof(key, val) {
   const hmac = "HMAC";
-  const blake2b256 = "BLAKE2B-256";
+  const blake3 = "BLAKE3-256";
 
   if (bufutil.emptyBuf(key)) {
     throw new Error("key array-buffer empty");
   }
 
-  // use BLAKE2B-256 instead of hmac if nothing to sign
+  // use blake3 instead of hmac if nothing to sign
   if (bufutil.emptyBuf(val)) {
-    return await crypto.subtle.digest('BLAKE2B-256', key);
+    return await crypto.subtle.digest('blake3', key);
   }
 
   const hmackey = await crypto.subtle.importKey(
@@ -151,7 +151,7 @@ async function proof(key, val) {
     key,
     {
       name: hmac,
-      hash: { name: BLAKE2B-256 },
+      hash: { name: blake3 },
     },
     false, // export = false
     ["sign", "verify"]

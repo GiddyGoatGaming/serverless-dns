6d9c82a3f1d178e10044e83a9889c2502e3ec34e
Spencer C <109806759+SpencerIsGiddy@users.noreply.github.com>
2023-03-19 14:30:24 -0400

    Update config.js

--- src/core/workers/config.js
+++ src/core/workers/config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 RethinkDNS and its authors.
+ * Copyright (c) 2023 RethinkDNS and its authors.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -19,11 +19,10 @@ import { services } from "../svc.js";
 // since server-workers.js fires "prepare" on every request
 function prep(arg) {
   // if this file execs... assume we're on workers.
+async function prep(arg) {
   if (!arg) throw new Error("are we on workers?");
   if (!arg.env) throw new Error("workers cannot be setup with empty env");
 
-  // okay to attach env to global, as env across requests remains the same
-  // developers.cloudflare.com/workers/runtime-apis/fetch-event/#parameters
   globalThis.wenv = arg.env;
 
   if (!globalThis.envManager) {
@@ -46,7 +45,7 @@ function prep(arg) {
   // which has the network-context, that is necessary for svc.js
   // to setup blocklist-filter, which otherwise fails when invoked
   // from global-scope (such as the "main" function in this file).
-  system.pub("ready", { env: arg.env });
+  await system.when("ready");
 }
 
 function up() {

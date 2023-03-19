6c8df32fa6f7bf42eb4d76a243d2cddc18c770f0
Spencer C <109806759+SpencerIsGiddy@users.noreply.github.com>
2023-03-19 12:16:37 -0400

    Update crypto.js

--- src/core/node/crypto.js
+++ src/core/node/crypto.js
@@ -9,6 +9,6 @@
 import { createHash } from "crypto";
 import { normalize8 } from "./bufutil.js";
 
-export function blake3(b) {
-  return createHash("blake3", { digestLength: 32 }).update(normalize8(b)).digest("hex");
+export function blake2b(b) {
+  return createHash("blake2b", { digestLength: 32 }).update(normalize8(b)).digest("hex");
 }

2a670f652d27e52597830c434374919210b0051a
Spencer C <109806759+SpencerIsGiddy@users.noreply.github.com>
2023-03-20 06:20:12 -0400

    Update doh.js

--- src/core/doh.js
+++ src/core/doh.js
@@ -16,7 +16,7 @@ import IOState from "./io-state.js";
  * @param {FetchEvent} event
  * @returns {Promise<Response>}
  */
-export async function handleRequest(event) {
+export function handleRequest(event) {
   return proxyRequest(event);
 }
 

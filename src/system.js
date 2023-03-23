b024e96058b24fae62eda209880ea3c2390d8a15
Spencer C <109806759+SpencerIsGiddy@users.noreply.github.com>
2023-03-23 17:24:45 -0400

    Update system.js

--- src/system.js
+++ src/system.js
@@ -7,121 +7,101 @@
  */
 import * as util from "./commons/util.js";
 
-// Evaluate if EventTarget APIs can replace this hand-rolled impl
-// developers.cloudflare.com/workers/platform/changelog#2021-09-24
-// once emitted, they stick; firing off new listeners forever, just the once.
-const stickyEvents = new Set([
-  // when process bring-up is done
+const stickyEvents = Object.freeze(new Set([
   "prepare",
-  // when env setup is done
   "ready",
-  // when svc setup is done
   "steady",
-  // when all systems are a-go
   "go",
-]);
+]));
 
-const events = new Set([
-  // when server should cease
+const events = Object.freeze(new Set([
   "stop",
-]);
+]));
 
-const listeners = new Map();
-const waitGroup = new Map();
+const listeners = Object.freeze(new Map(
+  [...events, ...stickyEvents].map((event) => [event, new Set()])
+));
 
-(() => {
-  for (const e of events) {
-    listeners.set(e, new Set());
-    waitGroup.set(e, new Set());
-  }
+const waitGroup = Object.freeze(new Map(
+  [...events, ...stickyEvents].map((event) => [event, new Set()])
+));
 
-  for (const se of stickyEvents) {
-    listeners.set(se, new Set());
-    waitGroup.set(se, new Set());
-  }
-})();
-
-// fires an event
-export function pub(event, parcel = undefined) {
+function pub(event, parcel = undefined) {
   awaiters(event, parcel);
   callbacks(event, parcel);
 }
 
-// invokes cb when event is fired
-export function sub(event, cb) {
+function sub(event, cb) {
   const eventCallbacks = listeners.get(event);
-
-  // if such even callbacks don't exist
   if (!eventCallbacks) {
-    // but event is sticky, fire off the listener at once
     if (stickyEvents.has(event)) {
       util.microtaskBox(cb);
       return true;
     }
-    // but event doesn't exist, then there's nothing to do
     return false;
   }
-
   eventCallbacks.add(cb);
-
   return true;
 }
 
-// waits till event fires or timesout
-export function when(event, timeout = 0) {
+function when(event, timeout = 0) {
   const wg = waitGroup.get(event);
-
   if (!wg) {
-    // if stick event, fulfill promise right away
     if (stickyEvents.has(event)) {
       return Promise.resolve(event);
-    }
-    // no such event
-    return Promise.reject(new Error(event + " missing"));
+   .reject(new Error(`${event} missing`));
   }
-
   return new Promise((accept, reject) => {
-    const tid =
-      timeout > 0
-        ? util.timeout(timeout, () => {
-            reject(new Error(event + " elapsed " + timeout));
-          })
-        : -2;
+    const tid = timeout > 0
+      ? util.timeout(timeout, () => {
+          reject(new Error(`${event} elapsed ${timeout}`));
+        })
+      : -2;
     const fulfiller = function (parcel) {
       if (tid >= 0) clearTimeout(tid);
       accept(parcel, event);
     };
     wg.add(fulfiller);
+    Promise.race([
+      new Promise((_, reject) => {
+        if (timeout > 0) {
+          util.timeout(timeout, () => {
+            reject(new Error(`${event} elapsed ${timeout}`));
+          });
+        }
+      }),
+      new Promise((resolve) => {
+        const cleanup = () => {
+          wg.delete(fulfiller);
+          if (wg.size === 0) {
+            waitGroup.delete(event);
+          }
+        };
+        fulfiller.cleanup = cleanup;
+        fulfiller.resolve = resolve;
+      }),
+    ]).finally(() => {
+      cleanup();
+    });
   });
 }
 
 function awaiters(event, parcel) {
   const g = waitGroup.get(event);
-
   if (!g) return;
-
-  // listeners valid just the once for stickyEvents
   if (stickyEvents.has(event)) {
     waitGroup.delete(event);
   }
-
   util.safeBox(g, parcel);
 }
 
 function callbacks(event, parcel) {
   const cbs = listeners.get(event);
-
   if (!cbs) return;
-
-  // listeners valid just the once for stickyEvents
   if (stickyEvents.has(event)) {
     listeners.delete(event);
   }
-
-  // callbacks are queued async and don't block the caller. On Workers,
-  // where IOs or timers require event-context aka network-context,
-  // which is only available when fns are invoked in response to an
-  // incoming request (through the fetch event handler), such callbacks
-  // may not even fire. Instead use: awaiters and not callbacks.
   util.microtaskBox(cbs, parcel);
 }
+
+export { pub, sub, when };

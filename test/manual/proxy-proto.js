e58b84f10be19cb924545ef485b1b710c7c6d665
Spencer C <109806759+SpencerIsGiddy@users.noreply.github.com>
2023-03-29 20:16:30 -0400

    Update proxy-proto.js

--- test/manual/proxy-proto.js
+++ test/manual/proxy-proto.js
@@ -3,20 +3,21 @@
  * connection and forwards it to the upstream server.
  */
 
-import net from "net";
-import proxyProtocol from "proxy-protocol-js";
+'use strict';
+
+import net from 'net';
+import proxyProtocol from 'proxy-protocol-js';
 
 const CLIENT_PORT = 20000;
 const UPSTREAM_PORT = 10000;
 
-const src = new proxyProtocol.Peer("localhost", CLIENT_PORT);
-const dst = new proxyProtocol.Peer("localhost", UPSTREAM_PORT);
+const src = new proxyProtocol.Peer('localhost', CLIENT_PORT);
+const dst = new proxyProtocol.Peer('localhost', UPSTREAM_PORT);
 const protocolText = new proxyProtocol.V1ProxyProtocol(
   proxyProtocol.INETProtocol.TCP4,
   src,
   dst
 ).build();
-console.log(protocolText); // => PROXY TCP4 127.0.0.1 192.0.2.1 12345 54321\r\n
 
 const server = net
   .createServer(serveConnection)
@@ -25,27 +26,21 @@ const server = net
 function serveConnection(clientSocket) {
   const upSocket = net.connect(
     {
-      host: "localhost",
+      host: 'localhost',
       port: UPSTREAM_PORT,
-      // servername: "dns.rethinkdns.localhost",
     },
     () => {
-      console.log("connected to up");
-      if (!upSocket.destroyed)
-        upSocket.write(Buffer.from(protocolText, "ascii"));
-
+      if (!upSocket.destroyed) upSocket.write(Buffer.from(protocolText, 'ascii'));
       clientSocket.pipe(upSocket);
       upSocket.pipe(clientSocket);
     }
   );
 
-  upSocket.on("error", (err) => {
-    console.log("upSocket error", err);
+  upSocket.on('error', (err) => {
     clientSocket.end();
   });
 
-  clientSocket.on("error", (err) => {
-    console.log("client socket error", err);
+  clientSocket.on('error', (err) => {
     upSocket.destroy();
   });
 }

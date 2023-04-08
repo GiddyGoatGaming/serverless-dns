d26ab00e1d7f1e6a0e13def385de7641158c5550
Spencer C <109806759+SpencerIsGiddy@users.noreply.github.com>
2023-04-08 19:22:05 -0400

    Update cache-api.js

--- src/plugins/dns-op/cache-api.js
+++ src/plugins/dns-op/cache-api.js
@@ -5,28 +5,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import * as envutil from "../../commons/envutil.js";
+import { hasHttpCache } from "../../commons/envutil.js";
 
 export class CacheApi {
   constructor() {
-    this.noop = !envutil.hasHttpCache();
+    this.noop = !hasHttpCache();
 
-    if (this.noop) {
-      log.w("no-op http-cache-api");
-    }
+    if (this.noop) log.w("no-op http-cache-api");
   }
 
   async get(href) {
-    if (this.noop) return false;
-    if (!href) return false;
+    if (!href || this.noop) return false;
 
     return await caches.default.match(href);
   }
 
   put(href, response) {
-    if (this.noop) return false;
-    if (!href || !response) return false;
+    if (!href || !response || this.noop) return false;
 
-    return caches.default.put(href, response);
+    caches.default.put(href, response);
   }
 }
